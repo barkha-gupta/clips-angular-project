d349ea0844101f1405604e005a53e8af0189e780
barkha-gupta <barkhagupta401@gmail.com>
2023-10-25 07:22:42 +0530

    adding files

---
+++ src/app/user/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  credentials = {
+    email : '',
+    password : ''
+  }
+
+  login(){
+    alert('logged in')
+  }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    prduction: true,
+    firebase: {
+        apiKey: "AIzaSyA7744YizRj3tUDo9-kHeGqqKS9q6zfWzI",
+        authDomain: "clips-b3e77.firebaseapp.com",
+        projectId: "clips-b3e77",
+        storageBucket: "clips-b3e77.appspot.com",
+        messagingSenderId: "1055679980906",
+        appId: "1:1055679980906:web:9c776487a3e8915fc17113"
+    }
+};
